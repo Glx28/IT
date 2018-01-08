a34f17c793acb37d665f36bfd285c80bc79cd340
Melhus <some3@elev.vaf.no>
2018-01-08 10:07:00 +0100

    nettsiden

---
+++ nettsiden/ok.php
@@ -0,0 +1,14 @@
+        <?php
+        $tall = 23;
+        echo "<p>Nå er $tall. </p>";
+        
+        $tall = 42;
+        echo "<p>Nå er tallet $tall. </p>";
+        ?>
+        
+        <p> my daddy works for Trump </p>
+
+        <?php
+        echo "<p> her er vi tilbake i php, da må jeg bruke echo for å skrive Html-kode. </p>";
+        echo "<p> variabelen jeg definerte i starten av dokumentet, finnes fremdeles, tallet er $tall. </p>";
+        ?>
